63e367464b7888472126e3932d3131d432107708
DESKTOP-COL9V4V\Jei <jeiyang108@gmail.com>
2018-03-26 20:45:04 -0700

    Add project files.

---
+++ LymcWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LymcWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
